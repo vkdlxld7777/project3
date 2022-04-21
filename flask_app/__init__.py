c747f937d34758ee7cfb1200fbf91ca70bc28a31
vkdlxld7777 <vkdlxld7777@gmail.com>
2022-04-21 16:43:44 +0900

    [긴급 수정]

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -39,6 +39,29 @@ def create_app():
         data = db.game_list()
         json_data = {'game' : data}
         return json_data
+    
+    @app.rout('/game_list/<game_name>')
+    def game(game_name):
+        data = db.game_data(game_name)
+        json_data = []
+        for i in data:
+            json_data += [{
+                "ranking" : i[0],
+                "game" : i[1],
+                "month" : i[2],
+                "year" : i[3],
+                "hours_watched" : i[4],
+                "hours_streamed" : i[5],
+                "peak_viewers" : i[6],
+                "peak_channels" : i[7],
+                "streamers" : i[8],
+                "avg_viewers" : i[9],
+                "avg_channels" : i[10],
+                "avg_viewer_ratio" : i[11]
+            }]
+        json_dump = json.dumps({'data':json_data})
+        json_data = json.loads(json_dump)
+        return json_data, 200
     return app
 
 
